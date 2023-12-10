db037dacc9ecd52b067eea65ec101f366b5e008f
王海涛 <859926750@qq.com>
2023-12-10 13:37:31 +0800

    refactor: 更新go版本，docker导出配置文件

--- main.go
+++ main.go
@@ -24,10 +24,10 @@ func main() {
 }
 
 func initConfig() {
-	viper.SetConfigName("config") // 配置文件名字，注意没有扩展名
-	viper.SetConfigType("yaml")   // 如果配置文件的名称中没有包含扩展名，那么该字段是必需的
-	viper.AddConfigPath(".")      // 在当前工作目录寻找配置文件
-	err := viper.ReadInConfig()   // 查找并读取配置文件
+	viper.SetConfigName("config")   // 配置文件名字，注意没有扩展名
+	viper.SetConfigType("yaml")     // 如果配置文件的名称中没有包含扩展名，那么该字段是必需的
+	viper.AddConfigPath("./Config") // 在当前工作目录寻找配置文件
+	err := viper.ReadInConfig()     // 查找并读取配置文件
 	if err != nil {
 		panic(errors.Errorf("Fatal error config file: %v \n", err))
 	}
